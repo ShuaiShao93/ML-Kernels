b38a79d2a59e899491d9ed03cf4949666ad86065
Shuai Shao <shshao@nuro.ai>
2023-10-05 11:48:12 -0700

    direct io in trt conv3d

--- trt/conv3d.py
+++ trt/conv3d.py
@@ -26,18 +26,38 @@ trt_logger = trt.Logger(trt.Logger.VERBOSE)
 builder = trt.Builder(trt_logger)
 
 network = builder.create_network(EXPLICIT_BATCH)
-input = network.add_input(name="input", shape=INPUT_SHAPE, dtype=trt.float32)
+input = network.add_input(name="input", shape=INPUT_SHAPE, dtype=trt.float16)
+input.allowed_formats = 1 << int(trt.TensorFormat.DHWC8)
 conv3d = network.add_convolution_nd(
-    input, OC, [KD, KH, KW], np.ones(KERNEL_SHAPE, dtype=np.float32))
+    input, OC, [KD, KH, KW], np.ones(KERNEL_SHAPE, dtype=np.float16))
 conv3d.padding_nd = (1, 1, 1)
 conv3d.stride_nd = (1, 1, 1)
+
+# Useless with trt.BuilderFlag.FP16 below
+# conv3d.precision = trt.float16
+
+# Seems useless with "output.dtype = trt.DataType.HALF" below
+# conv3d.set_output_type(0, trt.DataType.HALF)
+
 output = conv3d.get_output(0)
 output.name = "output"
+output.dtype = trt.DataType.HALF # required
+output.allowed_formats = 1 << int(trt.TensorFormat.DHWC8)
 network.mark_output(output)
-# assert output.shape == OUTPUT_SHAPE
+assert output.shape == OUTPUT_SHAPE
 
 config = builder.create_builder_config()
+config.set_flag(trt.BuilderFlag.FP16)
+config.set_flag(trt.BuilderFlag.DIRECT_IO)
+config.profiling_verbosity = trt.ProfilingVerbosity.VERBOSE
+
 engine = builder.build_engine(network, config)
+inspector = engine.create_engine_inspector()
+# Row major means channel first, channel major means channel last.
+print('trt_engine layer_info:\n{}'.format(
+    inspector.get_engine_information(trt.LayerInformationFormat.JSON)
+    ))
+
 context = engine.create_execution_context()
 
 names = [_ for _ in engine]
@@ -49,13 +69,13 @@ bindings = [None] * num_bindings
 inputs = {}
 for name in input_names:
     input_idx = engine.get_binding_index(name)
-    inputs[name] = cuda.mem_alloc(INPUT_SIZE * 4)
+    inputs[name] = cuda.mem_alloc(INPUT_SIZE * 2)
     bindings[input_idx] = int(inputs[name])
 
 outputs = {}
 for name in output_names:
     output_idx = engine.get_binding_index(name)
-    outputs[name] = cuda.mem_alloc(OUTPUT_SIZE * 4)
+    outputs[name] = cuda.mem_alloc(OUTPUT_SIZE * 2)
     bindings[output_idx] = int(outputs[name])
 
 stream = cuda.Stream()
